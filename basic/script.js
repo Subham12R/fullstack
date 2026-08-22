d411f4b811c0c676b3b48f19c9e2cfc13681284a
Subham Karmakar <rikk4335@gmail.com>
2026-08-22 09:28:37 +0530

    Init repo

---
+++ basic/script.js
@@ -0,0 +1,13 @@
+console.log("hello")
+
+let name = prompt("Enter your name:")
+
+console.log(name)
+
+const button = document.getElementById("btn-1")
+
+button.addEventListener('click', () => {
+    alert("Hi!");
+}
+
+)
---
+++ start/script.js
@@ -0,0 +1,14 @@
+console.log('Hello this is JS principles')
+
+let x = prompt("Enter name: ")
+console.log(x)
+if (x != null) {
+    alert("Hello")
+}
+
+console.log(eval("5+3"))
+
+for(i = 0; i < 10; i++){
+    console.log(eval("1+i"))
+}
+
